106d1991919bf7f51e3849aee3bc3871a3b05bad
kihuni <stephenkihuni55@gmail.com>
2024-11-23 04:43:03 +0300

    fix:UndefinedValueError

--- collabSphere/settings.py
+++ collabSphere/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='fallback-secret-key-for-ci')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-AUTH_USER_MODEL = 'users.User'
+
 
 
 ROOT_URLCONF = "collabSphere.urls"
@@ -122,6 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
